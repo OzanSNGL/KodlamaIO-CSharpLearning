4bb800c7b4df8697020f844f7b46bbfaf190b278
Ozan Şengül <ozn.sngl.ieu@hotmail.com>
2021-01-20 23:58:26 +0300

    New subjects

---
+++ Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Ozan", "Umut", "Tuncay", "Defne" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("Ezgi");
+            Console.WriteLine(isimler[4]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product { Id = 1, CategoryId = 101, ProductName ="Masa", UnitInStock = 500, UnitPrice = 300 };
+            Product product2 = new Product {Id = 2, CategoryId = 201, ProductName="Sandalye", UnitInStock= 430, UnitPrice = 250 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+        }
+    }
+}
