6dca447cee66f13aa7b5b8de9692a1b7d9f27029
Ozan Şengül <ozn.sngl.ieu@hotmail.com>
2021-01-20 23:57:58 +0300

    Added a menu.

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,80 +6,103 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            Customer customer1 = new Customer();
-            customer1.Id = 1000001;
-            customer1.Name = "John";
-            customer1.Surname = "Foley";
-            customer1.Working = true;
+            #region customers
+            Customer customer1 = new Customer { Id = 1000001, Name = "John", Surname = "Foley", Working = true};
 
-            Customer customer2 = new Customer();
-            customer2.Id = 1000002;
-            customer2.Name = "Lacy";
-            customer2.Surname = "Evans";
-            customer2.Working = false;
+            Customer customer2 = new Customer { Id = 1000002, Name = "Lacy", Surname = "Evans", Working = false};
 
-            Customer customer3 = new Customer();
-            customer3.Id = 1000003;
-            customer3.Name = "Victor";
-            customer3.Surname = "Oladipo";
-            customer3.Working = true;
+            Customer customer3 = new Customer { Id = 1000003, Name = "Victor", Surname = "Oladipo", Working = true };
 
-            Customer customer4 = new Customer();
-            customer4.Id = 1000004;
-            customer4.Name = "Blake";
-            customer4.Surname = "Mannings";
-            customer4.Working = false;
+            Customer customer4 = new Customer { Id = 1000004, Name = "Blake", Surname = "Manning", Working = false };
 
             Customer[] allCustomers = new Customer[] { customer1, customer2, customer3, customer4 };
+            #endregion
 
             CustomerManager customerManager = new CustomerManager();
 
-
-
+            #region yaz-customers
             foreach (var x in allCustomers)
             {
                 customerManager.Add(x);
                 Console.WriteLine("--------");
             }
 
-            Console.WriteLine("Non-Retired Customers: \n__________");
+            Console.WriteLine("Non-Retired Customers: \n__");
 
             for (int j = 0; j < allCustomers.Length; j++)
             {
                 if (allCustomers[j].Working == true)
                 {
                     customerManager.ListAll(allCustomers[j]);
-                    Console.WriteLine("*****");
+                    Console.WriteLine("*");
 
                 }
             }
+            #endregion
 
 
-            Console.WriteLine(" ");
+        start: Console.WriteLine("Please choose one of the following: \r\n 1. Update a customer \r\n 2. Delete a customer");
+            string o1 = null;
+            o1 = Console.ReadLine();
 
-            start: Console.WriteLine("Please enter an ID Code:");
-
-            int Number = new int();
-            Number = Convert.ToInt32(Console.ReadLine());
-
-            bool foundIt = new bool();
-
-            for (int i = 0; i < allCustomers.Length; i++)
+            switch (o1)
             {
-                if (Number == allCustomers[i].Id)
-                {
-                    foundIt = true;
-                    customerManager.Delete(allCustomers[i]);
+                #region case 1
+                case "1":
+                    Console.WriteLine("Please enter an ID Code: ");
+                    int enteredNumber = new int();
+                    enteredNumber = int.Parse(Console.ReadLine());
+                    for (int a = 0; a < allCustomers.Length; a++)
+                    {
+                        if (enteredNumber == allCustomers[a].Id)
+                        {
+                            Console.WriteLine("Found : {0} {1}", allCustomers[a].Name, allCustomers[a].Surname);
+                            Console.WriteLine("Please enter a new name: ");
+                            allCustomers[a].Name = Console.ReadLine().ToString();
+                            Console.WriteLine("You have successfully changed name.");
+                            goto start;
+                        }
+                    }
+                    Console.WriteLine("Couldn't find the ID you have entered : {0}", enteredNumber);
                     goto start;
-                }
-                foundIt = false;
+                #endregion
+                #region case 2
+                case "2":
+                    Console.WriteLine("Please enter an ID Code: ");
+                    int enteredNumber2 = new int();
+                    enteredNumber2 = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < allCustomers.Length; i++)
+                    {
+                        if (enteredNumber2 == allCustomers[i].Id)
+                        {
+                        yesorno: Console.WriteLine("Are you sure to delete " + allCustomers[i].Name + " " + allCustomers[i].Surname + "?" + "Y/N");
+                            string yes = "Y";
+                            string no = "N";
+                            string o2 = null;
+                            o2 = Console.ReadLine();
+                            if (o2 == yes || o2 == no)
+                            {
+                                switch (o2)
+                                {
+                                    case "Y":
+                                        customerManager.Delete(allCustomers[i]);
+                                        goto start;
+                                    case "N":
+                                        Console.WriteLine(allCustomers[i].Name + allCustomers[i].Surname + "will not be deleted.");
+                                        goto start;
+                                }
+                            }
+                            Console.WriteLine("Please use either Y or N.");
+                            goto yesorno;
+                        }
 
+                    }
+                    Console.WriteLine("You have entered an invalid ID code.");
+                    goto start;
+                    #endregion
             }
-            if (foundIt == false)
-            {
-                Console.WriteLine("You have entered an invalid ID Code.");
-                goto start;
-            }
+
+        }
         }
     }
-}
+
