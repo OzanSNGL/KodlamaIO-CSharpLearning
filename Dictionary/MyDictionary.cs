e75a858612c362d1f9d473dd0c6e953a66802b2a
Ozan Şengül <ozn.sngl.ieu@hotmail.com>
2021-01-22 01:04:29 +0300

    New HW

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    
+    class MyDictionary<T1, T2>
+    {
+        T1[] words;
+        T2[] keys;
+        public MyDictionary()
+        {
+            words = new T1[0];
+            keys = new T2[0];
+        }
+
+        public void Add(T1 word, T2 key)
+        {
+            T1[] _word = words;
+            T2[] _key = keys;
+            words = new T1[words.Length + 1];
+            keys = new T2[keys.Length + 1];
+            for (int i = 0; i < _word.Length; i++)
+            {
+                words[i] = _word[i];
+                keys[i] = _key[i];
+            }
+            words[words.Length - 1] = word;
+            keys[keys.Length - 1] = key;
+
+        }
+
+        public T1[] Words
+        {
+            get { return words; }
+        }
+        public T2[] Keys
+        {
+            get { return keys; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> names = new MyDictionary<int, string>();
+            names.Add(01, "Ozan");
+            names.Add(02, "Defne");
+            names.Add(03, "Şans");
+            names.Add(04, "Hera");
+            names.Add(05, "Umut");
+            names.Add(06, "Tuncay");
+            names.Add(07, "Benek");
+
+
+
+            foreach (var a in names.Words )
+            {
+                Console.WriteLine(a);
+            }
+
+            foreach (var b in names.Keys)
+            {
+                Console.WriteLine(b);
+            }
+
+        }
+    }
+}
